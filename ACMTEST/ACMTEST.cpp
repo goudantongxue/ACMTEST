e30e9cde5bc2f8e4446a1cc3c917daaefb9734c0
Andrew Shi <shiqishuo0913@163.com>
2022-08-22 21:16:11 +0800

    220822

--- ACMTEST/ACMTEST.cpp
+++ ACMTEST/ACMTEST.cpp
@@ -2,15 +2,6 @@
 using namespace std;
 
 
-/*
-* 为什么选择这个职位？
-* 对oppo有什么了解？
-* 对oppo手机有什么了解？
-* 自己的优缺点？
-* 
-*/
-
-
 namespace ABOUTVECTOR
 {
 	void STLResize()
@@ -506,16 +497,20 @@ namespace BFS
 				{
 					q.push(make_pair(x, y));
 
-					// 处理树边
+					// 处理无向图和有向图的树边
 					ProcessTreeEdge();
 
 					IsDiscovered[x][y] = true;
 					ParentTable[x][y] = make_pair(tmp.first, tmp.second);
 				}
-				else if (!IsProcessed[x][y] || isDirected)
+				else if (!IsProcessed[x][y])
 				{
-					// 处理树边之外的其他边
+					// 处理有向图和无向图中非树边，分类为已发现但未处理完
 					ProcessOtherEdge();
+				}
+				else
+				{
+					// 主要用于处理已经处理完的边，这在无向图中没有什么意义，但在有向图中可以进行一些特殊的处理。
 				}
 			}
 
@@ -819,12 +814,7 @@ namespace DIRECTEDGRAPHSEARCH
 	// 所有这个代码段主要来探讨如何做有向图的搜索
 	
 	// 正确选择搜索的起点非常重要，最好只从正确的起点进行一次搜索就解决问题，因为对于单一节点开始的搜索
-	// 有向图和无向图的深度优先搜索并无太大区别，但是要对整张图进行全搜索，有向图和无向图就会表现出一定的区别
-	// 比如说，对于这道题我发现的区别是：无向图只要节点之间有链接，就一定可以搜索到，所以对于多连通图来说，
-	// 多次搜索并不会遇到相同的节点；但对于有向图来说，有时候两个节点间尽管可能有链接，但常常由于连接是单向的
-	// 在多联通图中，有可能两次搜索会遇到同一节点，这时候恰当的设置全局图搜索状态就变得有些关键。
-	// 
-	// 
+
 	// 题目一
 	/*
 	5 5
@@ -838,9 +828,8 @@ namespace DIRECTEDGRAPHSEARCH
 	int endX, endY;
 	int totalNum = 0;
 
-	vector<vector<bool>> isvisted(n, vector<bool>(m, false));
-
-	vector<vector<char>> pos(n, vector<char>(m, ' '));
+	vector<vector<bool>> isvisted;
+	vector<vector<char>> pos;
 
 	queue<pair<int, int>>achPos;//可达位置
 
@@ -895,12 +884,112 @@ namespace DIRECTEDGRAPHSEARCH
 
 	}
 
+	// 上面的方案重点在于选对了开始的起点，这样只需进行一次搜索就可以。但是我们能否对所有节点均进行搜索呢？
+	// 答案是可以，但搜索之前要对一些数据结构做必要的初始化
+	// 以及对于 isProcessed 标志为 TRUE 节点的运用
+	// 有向图和无向图的深度优先搜索并无太大区别，但是要对整张图进行全搜索，有向图和无向图就会表现出一定的区别
+	// 比如说，对于这道题我发现的区别是：无向图只要节点之间有链接，就一定可以搜索到，所以对于多连通图来说，
+	// 多次搜索并不会遇到相同的节点；但对于有向图来说，有时候两个节点间尽管可能有链接，但常常由于连接是单向的
+	// 在多联通图中，有可能两次搜索会遇到同一节点，这时候恰当的设置全局图搜索状态就变得有些关键。
+
+	vector<vector<bool>> mem;
+	vector<vector<bool>> isprocessed;
+	vector<char> dfsDirections{ 'D', 'U', 'R', 'L' };
+
+	bool DFSZOUCHU(int x, int y)
+	{
+		if (pos[x][y] == 'O' || mem[x][y] == true)
+		{
+			mem[x][y] = true;
+			isprocessed[x][y] = true;
+			isvisted[x][y] = true;
+			return true;
+		}
+
+		isvisted[x][y] = true;
+
+		bool flag = false;
+		for (int i = 0; i < directions.size(); i++)
+		{
+			int newx = x + directions[i].first;
+			int newy = y + directions[i].second;
+
+			if (newx < 0 || newx >= n || newy < 0 || newy >= m)
+			{
+				continue;
+			}
+
+			if (isvisted[newx][newy] == false)
+			{
+				if (dfsDirections[i] == pos[x][y] || pos[x][y] == '.')
+				{
+					flag |= DFSZOUCHU(newx, newy);
+				}
+			}
+			else if (isprocessed[newx][newy] == false)
+			{
+
+			}
+			else
+			{
+				if (dfsDirections[i] == pos[x][y] || pos[x][y] == '.')
+				{
+					flag |= mem[newx][newy];
+				}
+			}
+		}
+		
+		isprocessed[x][y] = true;
+
+		mem[x][y] = flag;
+
+		return flag;
+	}
+
+	void PrintStatus()
+	{
+		cout << "********************************************" << endl;
+		for (auto& v : isvisted)
+		{
+			for (auto b : v)
+			{
+				cout << (b ? "TRUE" : "FALSE") << ' ';
+			}
+			cout << endl;
+		}
+		cout << endl;
+		cout << endl;
+		for (auto& v : isprocessed)
+		{
+			for (auto b : v)
+			{
+				cout << (b ? "TRUE" : "FALSE") << ' ';
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+		cout << endl;
+
+		for (auto& v : mem)
+		{
+			for (auto b : v)
+			{
+				cout << (b ? "TRUE" : "FALSE") << ' ';
+			}
+			cout << endl;
+		}
+		cout << "********************************************" << endl;
+	}
+
 
 	int ZOUCHUMIGONG()
 	{
 		cin >> n >> m;
 
 		isvisted.resize(n, vector<bool>(m, false));
+		isprocessed.resize(n, vector<bool>(m, false));
+		mem.resize(n, vector<bool>(m, false));
 		pos.resize(n, vector<char>(m));
 
 		string perRow;
@@ -919,12 +1008,33 @@ namespace DIRECTEDGRAPHSEARCH
 			}
 		}
 
-		achPos.push(make_pair(endX, endY));
+		int result = 0;
 
-		int res = canSuccess();
+		// BFS + 选择正确的开始节点
+		//achPos.push(make_pair(endX, endY));
+		//int res = canSuccess();
+		//int result = n * m - res;
 
-		return n * m - res;
+
+		// DFS + 遍历所有节点
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				fill(isvisted.data(), isvisted.data()+m*n, false);
+				fill(isprocessed.data(), isprocessed.data()+m*n, false);
+
+				//PrintStatus();
+				if (!DFSZOUCHU(i, j)) result++;
+				
+			}
+		}
+
+		PrintStatus();
+
+		return result;
 	}
+	
 
 	int main()
 	{
@@ -933,18 +1043,114 @@ namespace DIRECTEDGRAPHSEARCH
 
 		return 0;
 	}
+}
 
+namespace DAGSORT
+{
+	// A C++ program to print topological
+	// sorting of a DAG
 
-	// 用深度优先遍历可以做吗？可以，但搜索之前要对一些数据结构做必要的初始化
-	// 以及对于 isProcessed 标志为 TRUE 节点的运用
+	// Class to represent a graph
+	class Graph {
+		// No. of vertices
+		int V;
 
+		// Pointer to an array containing adjacency listsList
+		list<int>* adj;
 
+		// A function used by topologicalSort
+		void topologicalSortUtil(int v, bool visited[],
+			stack<int>& Stack);
 
+	public:
+		// Constructor
+		Graph(int V);
 
+		// function to add an edge to graph
+		void addEdge(int v, int w);
 
+		// prints a Topological Sort of
+		// the complete graph
+		void topologicalSort();
+	};
 
-	// 拓扑排序怎么做？
+	Graph::Graph(int V)
+	{
+		this->V = V;
+		adj = new list<int>[V];
+	}
 
+	void Graph::addEdge(int v, int w)
+	{
+		// Add w to v’s list.
+		adj[v].push_back(w);
+	}
+
+	// A recursive function used by topologicalSort
+	void Graph::topologicalSortUtil(int v, bool visited[],
+		stack<int>& Stack)
+	{
+		// Mark the current node as visited.
+		visited[v] = true;
+
+		// Recur for all the vertices
+		// adjacent to this vertex
+		list<int>::iterator i;
+		for (i = adj[v].begin(); i != adj[v].end(); ++i)
+			if (!visited[*i])
+				topologicalSortUtil(*i, visited, Stack);
+
+		// Push current vertex to stack
+		// which stores result
+		Stack.push(v);
+	}
+
+	// The function to do Topological Sort.
+	// It uses recursive topologicalSortUtil()
+	void Graph::topologicalSort()
+	{
+		stack<int> Stack;
+
+		// Mark all the vertices as not visited
+		bool* visited = new bool[V];
+		for (int i = 0; i < V; i++)
+			visited[i] = false;
+
+		// Call the recursive helper function
+		// to store Topological
+		// Sort starting from all
+		// vertices one by one
+		for (int i = 0; i < V; i++)
+			if (visited[i] == false)
+				topologicalSortUtil(i, visited, Stack);
+
+		// Print contents of stack
+		while (Stack.empty() == false) {
+			cout << Stack.top() << " ";
+			Stack.pop();
+		}
+	}
+
+	// Driver Code
+	int main()
+	{
+		// Create a graph given in the above diagram
+		Graph g(6);
+		g.addEdge(5, 2);
+		g.addEdge(5, 0);
+		g.addEdge(4, 0);
+		g.addEdge(4, 1);
+		g.addEdge(2, 3);
+		g.addEdge(3, 1);
+
+		cout << "Following is a Topological Sort of the given "
+			"graph \n";
+
+		// Function Call
+		g.topologicalSort();
+
+		return 0;
+	}
 
 }
 
@@ -2571,7 +2777,8 @@ int main()
 	//FORPRATICE::main();
 	//ArrayTest::main();
 	//RECURSIVEWAY::main();
-	ABOUTVECTOR::main();
+	//ABOUTVECTOR::main();
+	DIRECTEDGRAPHSEARCH::main();
 	return 0;
 
 }
